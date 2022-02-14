c94bafe266e93a88adeb1d6b5ff437b08cd15d68
brianorora <brianorora@gmail.com>
2022-02-14 17:17:11 +0300

    Add database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create!(description: 'Hola')
+Message.create!(description: 'Salut')
+Message.create!(description: 'Ciao')
+Message.create!(description: 'Sasa')
+Message.create!(description: 'Hi')
